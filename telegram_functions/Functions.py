fc63700c44960f33a130e339b1b43f5087356ece
SudoOmbro <10577777@polimi.it>
2020-06-04 19:01:29 +0200

    trying out bash

--- telegram_functions/Functions.py
+++ telegram_functions/Functions.py
@@ -1,11 +1,19 @@
 from telegram import TelegramError
 
 
-def send_message(update, context, text):
+def send_message_safe(update, context, text):
     try:
         context.bot.send_message(
             chat_id=update.message.chat.id,
             text=text
         )
     except TelegramError as e:
+        print("can't send the message")
         print(e)
+
+
+def send_message(update, context, text):
+    context.bot.send_message(
+        chat_id=update.message.chat.id,
+        text=text
+    )
