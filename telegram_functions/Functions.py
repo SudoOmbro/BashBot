ed822cad23f0b560df2b6e220bcc2a38a3927601
SudoOmbro <10577777@polimi.it>
2020-06-04 20:57:39 +0200

    added more functions

--- telegram_functions/Functions.py
+++ telegram_functions/Functions.py
@@ -1,4 +1,4 @@
-from telegram import TelegramError
+from telegram import TelegramError, InlineKeyboardMarkup, ParseMode
 
 
 def send_message(update, context, text):
@@ -10,3 +10,25 @@ def send_message(update, context, text):
     except TelegramError as e:
         print("can't send the message")
         print(e)
+
+
+def send_message_ik(update, context, text, keyboard):  # send message with inline keyboard
+    try:
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        context.bot.send_message(
+            chat_id=update.effective_user.id,
+            text=text,
+            reply_markup=reply_markup,
+            parse_mode=ParseMode.MARKDOWN)
+    except TelegramError as e:
+        print("can't send the message")
+        print(e)
+
+
+def delete_callback_message(update, context):
+    try:
+        context.bot.delete_message(chat_id=update.effective_user.id,
+                                   message_id=update.callback_query.message.message_id
+                                   )
+    except TelegramError:
+        print("The message is too old to be deleted.")
