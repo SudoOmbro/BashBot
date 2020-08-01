c8f400043160fd93128ab4268ff55777c1c66fa7
SudoOmbro <10577777@polimi.it>
2020-08-01 16:53:47 +0200

    fixed bash bug

--- utils/bash.py
+++ utils/bash.py
@@ -34,7 +34,7 @@ class Shell:
                 # go down one level
                 sub_folders = [f.name for f in os.scandir(self.dir) if f.is_dir()]
                 if args_list[1] in sub_folders:
-                    if self.is_linux:
+                    if not self.is_linux:
                         if self.dir[-1] != "\\":
                             self.dir += "\\"
                         self.dir += args_list[1]
